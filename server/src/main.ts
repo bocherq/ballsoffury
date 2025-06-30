d96d58f258018823f8b443b2cb821f2f970aa73e
Dmitrii Bocharov <bocherq@gmail.com>
2025-06-30 20:19:09 +0400

    server: implement create and get user methods

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import * as path from 'path';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
   app.setGlobalPrefix('api');
   if (process.env.NODE_ENV === 'production') {
     const publicPath = path.join(__dirname, '..', 'public');
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.entity';
+import { CreateUserDTO } from './user.dto';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(
+        @InjectRepository(User)
+        private userRepository: Repository<User>,
+    ) { }
+
+    async createUser(createUserDto: CreateUserDTO): Promise<User | null> {
+        const existingUser = await this.userRepository.findOneBy({ email: createUserDto.email });
+        if (!createUserDto.email.includes(process.env.ALLOWED_EMAIL_DOMAIN ?? '') || existingUser) {
+            return null;
+        }
+
+        console.log('Creating user: ', createUserDto);
+
+        const user = this.userRepository.create(createUserDto);
+        return await this.userRepository.save(user);
+    }
+
+    async getUserById(id: number): Promise<User | null> {
+        return await this.userRepository.findOneBy({ id });
+    }
+}
