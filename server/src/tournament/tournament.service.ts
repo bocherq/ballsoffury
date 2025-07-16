f635596dc0c92f82696d48b57cfb470af0f528b1
Dmitrii Bocharov <bocherq@gmail.com>
2025-07-16 09:31:55 +0400

    server: add distribute player into groups method

--- server/src/tournament/tournament.service.ts
+++ server/src/tournament/tournament.service.ts
@@ -1,10 +1,11 @@
-import { Injectable, ConflictException, NotFoundException } from '@nestjs/common';
+import { Injectable, ConflictException, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { CreateTournamentDTO } from './tournament.dto';
 import { Tournament } from 'src/entities/tournament.entity';
 import { Player } from 'src/entities/player.entity';
 import { UserService } from 'src/user/user.service';
+import { Group } from 'src/entities/group.entity';
 @Injectable()
 export class TournamentService {
     constructor(
@@ -12,12 +13,14 @@ export class TournamentService {
         private tournamentRepository: Repository<Tournament>,
         @InjectRepository(Player)
         private playerRepository: Repository<Player>,
+        @InjectRepository(Group)
+        private groupRepository: Repository<Group>,
         private userService: UserService,
     ) {}
 
     async get() {
         return await this.tournamentRepository.find({
-            relations: ['players', 'matches'],
+            relations: ['players'],
         });
     }
 
@@ -30,7 +33,7 @@ export class TournamentService {
     async getById(id: number) {
         return await this.tournamentRepository.findOne({
             where: { id },
-            relations: ['players', 'players.user', 'matches'],
+            relations: ['players', 'players.user', 'matches', 'groups'],
         });
     }
 
@@ -54,5 +57,55 @@ export class TournamentService {
         console.log(`User ${existingUser.firstName} ${existingUser.lastName} ${existingUser.id} joined to tournament ${existingTournament.id}`);
 
         return await this.getById(tournamentId);
+    }
+
+    async distributePlayersIntoGroups(tournamentId: number, groupCount: number): Promise<void> {
+        const tournament = await this.getById(tournamentId);
+
+        if (!tournament) {
+            throw new NotFoundException('Tournament not found');
+        }
+
+        const players = [...tournament.players];
+
+        if (players.length < groupCount * 3 || groupCount < 1) {
+            throw new BadRequestException('Invalid group count');
+        }
+
+        players.sort((a, b) => b.user.rating - a.user.rating);
+
+        const groups: Group[] = [];
+        for (let i = 0; i < groupCount; i++) {
+            const group = this.groupRepository.create({
+            name: `Group ${String.fromCharCode(65 + i)}`,
+            tournament,
+            });
+            console.log('Create group: ', group);
+            groups.push(group);
+        }
+
+        await this.groupRepository.save(groups);
+        console.log('Groups was saved');
+
+        let direction = 1;
+        let groupIndex = 0;
+
+
+        for (const player of players) {
+            const group = groups[groupIndex];
+            player.group = group;
+
+            groupIndex += direction;
+
+            if (groupIndex === groupCount) {
+                groupIndex = groupCount - 1;
+                direction = -1;
+            } else if (groupIndex < 0) {
+                groupIndex = 0;
+                direction = 1;
+            }
+        }
+
+        await this.playerRepository.save(players);
     }
 }
